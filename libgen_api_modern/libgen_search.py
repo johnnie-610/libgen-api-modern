67092977c03e888610092fd9a211b793796ccd65
Johnnie <99084912+johnnie-610@users.noreply.github.com>
2025-01-08 22:41:55 +0300

    New Year

--- libgen_api_modern/libgen_search.py
+++ libgen_api_modern/libgen_search.py
@@ -1,8 +1,8 @@
 # Copyright (c) 2024 Johnnie
-# 
+#
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
-# 
+#
 # This file is part of the libgen-api-modern library
 
 # import asyncio
@@ -33,22 +33,21 @@ class LibgenSearch:
             List[Dict[str, str]]: A list of dictionaries representing the search results.
 
         Examples:
-            
+
             ```python
-            from libgen_api_modern import LibgenSearch
             await LibgenSearch.search_scientific_articles("python")
             ```
 
         """
         try:
             search_request = SearchRequest(query)
-            
+
             return await search_request.aggregate_scimag_data(proxy=proxy)
         except ValueError as e:
             raise ValueError(f"Search query is too short: {e}")
         except Exception as e:
             raise Exception(f"An error occurred during the search: {e}")
-    
+
     async def search_scientific_articles_filtered(
         self,
         query: str,
@@ -75,9 +74,8 @@ class LibgenSearch:
             List[Dict[str, str]]: A list of dictionaries representing the filtered search results.
 
         Examples:
-            
+
             ```python
-            from libgen_api_modern import LibgenSearch
             filters = {"year": "2020"}
             await LibgenSearch.search_scientific_articles_filtered("python", filters, exact_match=True)
             ```
@@ -85,7 +83,7 @@ class LibgenSearch:
         """
         try:
             search_request = SearchRequest(query)
-            
+
             results = await search_request.aggregate_scimag_data(proxy=proxy)
             return await LibgenSearch.__filter_results(results, filters, exact_match)
         except ValueError as e:
@@ -113,20 +111,19 @@ class LibgenSearch:
         Examples:
 
             ```python
-            from libgen_api_modern import LibgenSearch
             await LibgenSearch.search_fiction("python")
             ```
 
         """
         try:
             search_request = SearchRequest(query)
-            
+
             return await search_request.aggregate_fiction_data(proxy=proxy)
         except ValueError as e:
             raise ValueError(f"Search query is too short: {e}")
         except Exception as e:
             raise Exception(f"An error occurred during the search: {e}")
-    
+
     async def search_fiction_filtered(
         self,
         query: str,
@@ -156,7 +153,6 @@ class LibgenSearch:
         Examples:
 
             ```python
-            from libgen_api_modern import LibgenSearch
             filters = {"year": "2020"}
             await LibgenSearch.search_fiction_filtered("python", filters, exact_match=True)
             ```
@@ -173,7 +169,7 @@ class LibgenSearch:
             raise Exception(f"An error occurred during the search: {e}")
 
     @staticmethod
-    async def search(query: str, search_type: str = "def", proxy: str | None = None) -> List[Dict[str, str]]:
+    async def search(query: str, search_type: str = "def") -> List[Dict[str, str]]:
         """
         Searches for books based on the given query.
 
@@ -181,7 +177,6 @@ class LibgenSearch:
             query (str): The search query.
             search_type (str, optional): The type of search to perform. Defaults to "def".
                 -Options are: 'def', 'author(s)', 'title', 'series', 'publisher', 'year', 'language', 'isbn', 'md5.
-            proxy (str, optional): The proxy to use for the search. Defaults to None.
                 -Use http proxy only with no authentication.
 
         Raises:
@@ -194,20 +189,19 @@ class LibgenSearch:
         Examples:
 
             ```python
-            from libgen_api_modern import LibgenSearch
             await LibgenSearch.search("python")
             ```
 
         """
         try:
             search_request = SearchRequest(query, search_type=search_type)
-            
-            return await search_request.aggregate_request_data(proxy=proxy)
+
+            return await search_request.aggregate_request_data()
         except ValueError as e:
             raise ValueError(f"Search query is too short: {e}")
         except Exception as e:
             raise Exception(f"Error during search: {e}")
-    
+
     @staticmethod
     async def search_filtered(
         query: str,
@@ -259,7 +253,7 @@ class LibgenSearch:
         except Exception as e:
             raise Exception(f"Error during search or filtering: {e}")
 
-    
+
     @classmethod
     async def __filter_results(
         cls,
@@ -272,7 +266,7 @@ class LibgenSearch:
                 return item == filter_value
             else:
                 return filter_value.lower() in item.lower()
-        
+
         filtered_results = []
         for result in results:
             match_all_filters = True
@@ -284,8 +278,8 @@ class LibgenSearch:
                 else:
                     match_all_filters = False
                     break
-            
+
             if match_all_filters:
                 filtered_results.append(result)
-        
+
         return filtered_results
--- libgen_api_modern/search_request.py
+++ libgen_api_modern/search_request.py
@@ -1,235 +1,144 @@
 # Copyright (c) 2024 Johnnie
-# 
+#
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
-# 
+#
 # This file is part of the libgen-api-modern library
 
 import asyncio
 import re
-import tracemalloc
-from asyncio import sslproto
-from aiohttp import ClientSession
-from bs4 import BeautifulSoup
-from bs4.element import Tag
-
+import httpx
+from lxml import html
 from typing import Optional, List, Dict
-
-tracemalloc.start()
-setattr(sslproto._SSLProtocolTransport, "_start_tls_compatible", True)
-
+from typing import Optional, List, Dict
 
 
 class SearchRequest:
 
+    domains = ["libgen.is", "libgen.st", "libgen.rs"]  # List of fallback domains
+    used_domain = None
     col_names = [
-        "ID",
-        "Author(s)",
-        "Title",
-        "Series",
-        "Publisher",
-        "Edition",
-        "Year",
-        "Pages",
-        "Language",
-        "Size",
-        "ISBN",
-        "Periodical",
-        "City",
-        "Extension",
+        "ID", "Author(s)", "Title", "Publisher",
+        "Year", "Pages", "Language", "Size", "Extension"
     ]
 
     def __init__(self, query: str, search_type: Optional[str] = "def") -> None:
-      
         self.query = query
         self.search_type = search_type
-
         if len(self.query) < 3:
             raise ValueError("Query is too short")
-        
-    
 
-    async def resolve_mirrors(self, session: ClientSession, mirror: str, proxy: Optional[str] = None) -> Dict[str, str]:
-        MIRROR_SOURCES = ["GET", "Cloudflare", "IPFS.io"]
+    async def resolve_mirrors(self, client: httpx.AsyncClient, mirror: str) -> Dict[str, str]:
+        
 
-        if mirror is None:
-            raise ValueError("No mirror specified")
 
-        response = await (await session.get(mirror, proxy=proxy)).text()
-        soup = BeautifulSoup(response, "html.parser")
+    async def fetch_page(self, client: httpx.AsyncClient, url: str) -> str:
+        response = await client.get(url, timeout=10)
+        return response.text
 
-        links = soup.find_all("a", string=MIRROR_SOURCES)
-        download_links = {link.string: link["href"] for link in links}
-        
-        return download_links
-        
-        
-    async def resolve_download_link(
-        self, 
-        session: ClientSession, 
-        mirror_1: str | None = None,
-        mirror_2: str | None = None,
-        proxy: Optional[str] = None
-    ) -> str | None:
-        if mirror_1 is None and mirror_2 is None:
-            raise ValueError("No mirrors specified")
-        
-        download_link = None
-        if mirror_1 is not None and mirror_2 is None:
-            response = await (await session.get(mirror_1, proxy=proxy)).text()
-        
-            soup = BeautifulSoup(response, "html.parser")
+    async def get_search_page(self, client: httpx.AsyncClient, type: str = "default") -> str:
+        parsed_query = "+".join(self.query.split(" "))
+
+        for domain in self.domains:
+            search_url = {
+                "default": f"https://{domain}/search.php?req={parsed_query}&lg_topic=libgen&open=0&view=simple&res=100&phrase=1&column={self.search_type}",
+                "fiction": f"https://{domain}/fiction/?q={parsed_query}",
+                "scimag": f"https://{domain}/scimag/?q={parsed_query}"
+            }.get(type, "default")
 
             try:
-                download_link = soup.find("a", string="GET")["href"]
-            except AttributeError:
-                download_link = None
-            except Exception:
-                download_link = None
+                res = await self.fetch_page(client, search_url)
+                if res:
+                    self.used_domain = domain
+                    return res
+            except httpx.HTTPError:
+                print(f"Failed to fetch from {domain}, trying next...")
 
-        if  mirror_2 is not None and mirror_1 is None:
-            response = await (await session.get(mirror_2, proxy=proxy)).text()
-            soup = BeautifulSoup(response, "html.parser")
-            
-            try:
-                save_button = soup.find('button', string='↓ save')
-            
-                if save_button:
-                    onclick_attr = save_button.get('onclick')
-                    
-                    match = re.search(r"location\.href='(.*?)'", onclick_attr)
-                    
-                    if match:
-                        relative_url = match.group(1)
-                        
-                        download_link = 'https:' + relative_url
+        raise Exception("All LibGen mirrors are unreachable.")
 
-            except AttributeError:
-                download_link = None
-            except Exception:
-                download_link = None
-        
-        return download_link
-
-    async def strip_i_tag_from_soup(
-        self, soup: BeautifulSoup, /) -> None:
-      
-        # Find all <i> tags
-        subheadings: List[Tag] = await asyncio.get_event_loop().run_in_executor(None, soup.find_all, "i")
-        
-        # Remove each <i> tag
-        for subheading in subheadings:
-            subheading.decompose()
-
-    async def fetch_page(self, session: ClientSession, url, proxy: Optional[str] = None):
-        response = await (await session.get(url, proxy=proxy)).text()
-        return response
-    
-    
-    async def get_search_page(self, session: ClientSession, proxy: Optional[str] = None, type: Optional[str] = "default"):
-        parsed_query = "+".join(self.query.split(" "))  # type: str
-        match type:
-            case "default": # searches non-fiction/sci-tech
-                search_url = (
-                    f"https://libgen.is/search.php?"
-                    f"req={parsed_query}&"
-                    f"res=100&"
-                    f"view=detailed&"
-                    f"phrase=1&"
-                    f"column={self.search_type}"  # type: str
-                )
-            case "fiction": # searches fiction
-                search_url = f"https://libgen.is/fiction/?q={parsed_query}"
-            case "scimag": # searches scientific articles
-                search_url = f"https://libgen.is/scimag/?q={parsed_query}"
-        
-
-        return await (await session.get(search_url, proxy=proxy)).text()
-    
-
-    async def aggregate_request_data(self, proxy: Optional[str] = None) -> List[Dict[str, str]]:
-
-        async with ClientSession() as session:
-            search_page = await self.get_search_page(session, proxy=proxy)
-
-            soup = BeautifulSoup(search_page, "html.parser")
-            await self.strip_i_tag_from_soup(soup)
-
-            tables = soup.find_all("table", {"border": "0", "rules": "cols", "width": "100%"})
-
+    async def aggregate_request_data(self) -> List[Dict[str, str]]:
+        async with httpx.AsyncClient() as client:
+            search_page = await self.get_search_page(client)
+            # with open("search_page.html", "w") as f:
+                # f.write(search_page)
+            # return
+            tree = html.fromstring(search_page)
+            table = tree.xpath("//table[@width='100%']")
             raw_data = []
+            print(table)
+
+            rows = table[0].xpath(".//tr")[1:]
+            if not rows:
+                return "No results"
+
+
+            for row in rows:
+                book_data = {}
+                cells = row.xpath(".//td")
+                if len(cells) < 2:
+                    continue
+                for cell in cells:
+                    value = cell.text_content().strip()
+                    if not value:
+                        value = ""
+                    book_data[self.col_names[len(book_data)]] = value
+                    mirrors = {}
+                    m_sources = cell.xpath(".//a")
+                    if m_sources:
+                        for m_source in m_sources:
+                            mirrors[m_source.get('title')] = m_source.get('href')
+
+                    book_data["Mirrors"] = mirrors
+
+                raw_data.append(book_data)
+
+            return raw_data
+
+
+
+
+
+            tables = tree.xpath("//table[@border='0' and @rules='rows' and @width='100%' and @align='center']")
+
 
             for table in tables:
-                rows = table.find_all("tr")[1:] 
-
+                rows = table.xpath(".//tr")[1:]
                 if not rows:
                     continue
 
                 book_data = {}
-                img_src = None
-                mirror = None
+                cover = table.xpath(".//td[@rowspan]/a/img/@src")
+                book_data["Cover"] = f"https://{self.used_domain}{cover[0]}" if cover else None
 
-                first_row = rows[0]
-                title_td = first_row.find_all("td")[2]
-                title = title_td.get_text(strip=True)
-                book_data["Title"] = title
-                img_tag = first_row.find('img')
-                if img_tag:
-                    img_src = img_tag.get('src')
-                a_tag = first_row.find('a', href=True)
-                mirror = "https://libgen.is" + a_tag['href'] if a_tag else None
-                
+                title_link = table.xpath(".//tr[td/b]/td[3]//a")
+                if title_link:
+                    book_data["Title"] = title_link[0].text_content().strip()
+                    book_data["Mirror"] = f"https://{self.used_domain}{title_link[0].get('href')[2:] if title_link[0].get('href').startswith('../') else title_link[0].get('href')}"
 
-
-
-                # Extract other book data
                 for row in rows:
-                    cells = row.find_all("td")
+                    cells = row.xpath(".//td")
                     if len(cells) < 2:
-                        continue  # Skip rows with insufficient data
-
-                    label = cells[0].get_text(strip=True).replace(":", "")
+                        continue # Skip rows with less data
+                    label = cells[0].text_content().strip().replace(":", "")
+                    value = cells[1].text_content().strip()
                     if label in self.col_names:
-                        book_data[label] = cells[1].get_text(strip=True)
-                        if len(cells) > 3 and cells[2].get_text(strip=True).replace(":", "") in self.col_names:
-                            book_data[cells[2].get_text(strip=True).replace(":", "")] = cells[3].get_text(strip=True)
+                        book_data[label] = value
+                        if len(cells) > 3 and cells[2].text_content().strip().replace(":", "") in self.col_names:
+                            book_data[cells[2].text_content().strip().replace(":", "")] = cells[3].text_content().strip()
 
                 for col_name in self.col_names:
-                    if col_name not in book_data:
-                        book_data[col_name] = ""
-
-                if img_src:
-                    book_data['Cover'] = f"https://libgen.is{img_src}"
-                
-                book_data['Mirror'] = mirror
+                    book_data.setdefault(col_name, "")
 
                 raw_data.append(book_data)
 
-        
+            tasks = [self.resolve_mirrors(client, book["Mirror"]) for book in raw_data if "Mirror" in book]
+            mirror_results = await asyncio.gather(*tasks)
 
-
-            for book in raw_data:
-                if "Mirror" in book:
-                    mirror = book["Mirror"]
-                    if mirror:
-                        download_links = await self.resolve_mirrors(session, mirror=mirror, proxy=proxy)
-                    else:
-                        book["Direct Download Link"] = None
-                    for key, value in download_links.items():
-                        if key == "GET":
-                            book["Direct Download Link 1"] = value
-                        elif key == "Cloudflare":
-                            book["Direct Download Link 2"] = value
-                        elif key == "IPFS.io":
-                            book["Direct Download Link 3"] = value
-                del book["Mirror"]
-            
-        
+            for book, mirrors in zip(raw_data, mirror_results):
+                book.pop("Mirror", None)
+                book.update({f"Direct Download Link {i+1}": link for i, link in enumerate(mirrors.values())})
 
         return raw_data
-    
-    
-
 
     async def aggregate_fiction_data(self, proxy: Optional[str] = None) -> List[Dict[str, str]]:
         fiction_cols = [
@@ -246,7 +155,7 @@ class SearchRequest:
         async with ClientSession() as session:
             search_page_content = await self.get_search_page(session, type="fiction", proxy=proxy)
             soup = BeautifulSoup(search_page_content, "html.parser")
-            
+
             tbody = soup.find("tbody")
             fiction_data = []
             tasks = []
@@ -258,7 +167,7 @@ class SearchRequest:
 
                 r_soup_content = await self.fetch_page(session, link, proxy=proxy)
                 r_soup = BeautifulSoup(r_soup_content, "html.parser")
-                
+
                 book = {}
                 img_tag = r_soup.find('img', src=lambda x: x and x.startswith('/fictioncovers/'))
                 record = r_soup.find('table', class_='record')
@@ -287,7 +196,7 @@ class SearchRequest:
                         break
 
                 if mirror_link:
-                    
+
                     d_link = await self.resolve_download_link(session, mirror_link, proxy=proxy)
                     book["Direct_Download_Link"] = d_link if d_link else None
                 fiction_data.append(book)
@@ -298,71 +207,3 @@ class SearchRequest:
             await asyncio.gather(*tasks)
 
         return fiction_data
-    
-    async def aggregate_scimag_data(self, proxy: Optional[str] = None) -> List[Dict[str, str]]:
-        scimag_cols = [
-            "Title",
-            "Authors",
-            "DOI",
-            "Journal",
-            "Publisher",
-            "Year",
-            "Volume",
-            "Issue",
-            "Pages",
-            "File Size",
-            "ID"
-        ]
-
-        async with ClientSession() as session:
-            search_page = await self.get_search_page(session, type="scimag", proxy=proxy)
-            soup = BeautifulSoup(search_page, "html.parser")
-            tbody = soup.find("tbody")
-            scimag_data = []
-            tasks = []
-
-            async def process_row(row):
-                title_td = row.find_all('td')[1]
-                link_tag = title_td.find('a')
-                link = f"https://libgen.is{link_tag['href']}"
-
-                r_soup_content = await self.fetch_page(session, link, proxy=proxy)
-                r_soup = BeautifulSoup(r_soup_content, "html.parser")
-                
-                book = {}
-                record = r_soup.find('table', class_='record')
-                rs = record.find_all('tr')
-                for r in rs:
-                    cells = r.find_all('td')
-                    data = cells[0].get_text(strip=True).replace(":", "")
-                    if data in scimag_cols:
-                        book[data] = cells[1].get_text(strip=True)
-
-                download_links = record.find('ul', class_='record_mirrors').find_all('a')
-                
-                links = []
-
-                for link in download_links:
-                    
-                    if 'library.lol/scimag' in link['href']:
-                        mirror_1 = link['href']
-                        d_link = await self.resolve_download_link(session, mirror_1 = mirror_1, proxy=proxy)
-                        links.append(d_link)
-
-                    elif 'sci-hub.ru' in link['href']:
-                        mirror_2 = link['href']
-                        d_link = await self.resolve_download_link(session, mirror_2 = mirror_2, proxy=proxy)
-                        links.append(d_link)
-                
-                book["Direct_Download_Link_1"] = "" if link[0] is None else links[0]
-                book["Direct_Download_Link_2"] = "" if link[1] is None else links[1]
-
-                scimag_data.append(book)
-            for row in tbody.find_all('tr'):
-                tasks.append(process_row(row))
-
-            await asyncio.gather(*tasks)
-        
-        # TODO: Add Journal links
-
-        return scimag_data
