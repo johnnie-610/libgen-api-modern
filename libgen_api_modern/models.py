ef06c1dc6f6ef689d569c457f793f1dfd9b1c19c
Johnnie <99084912+johnnie-610@users.noreply.github.com>
2025-01-12 23:41:24 +0300

    update #3

--- libgen_api_modern/models.py
+++ libgen_api_modern/models.py
@@ -1,24 +1,5 @@
 from dataclasses import dataclass
 
-
-@dataclass(frozen=True)
-class BookData:
-    id: str
-    authors: tuple[str, ...]
-    title: str
-    series: str | None
-    publisher: str
-    year: str
-    pages: str
-    language: str
-    size: str
-    extension: str
-    isbn: str | None
-    edition: str | None
-    cover_url: str | None
-    download_url: str | None
-
-
 @dataclass(frozen=True)
 class BkData:
     id: str
@@ -34,3 +15,27 @@ class BkData:
     mirrors: dict[str, str]
     isbn: str | None = None
     edition: str | None = None
+
+
+@dataclass(frozen=True)
+class BookData:
+    authors: tuple[str, ...]
+    title: str
+    publisher: str | None
+    year: str | None
+    pages: str | None
+    language: str | None
+    size: str | None
+    extension: str | None
+    isbn: str | None
+    cover_url: str | None
+    download_url: str | None
+
+
+
+@dataclass(frozen=True)
+class SearchResults:
+    total_results: int
+    books: list[BookData]
+    next_page_url: str | None
+    current_page: int
